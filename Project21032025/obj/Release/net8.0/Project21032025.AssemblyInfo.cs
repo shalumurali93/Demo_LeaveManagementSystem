cc5aec7a374442de97a2d27407e0e5f4a1e94759
Achu-Exp <achu73743@gmail.com>
2025-03-24 15:15:42 +0530

    clerared

--- Project21032025/obj/Release/net8.0/Project21032025.AssemblyInfo.cs
+++ Project21032025/obj/Release/net8.0/Project21032025.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project21032025")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f26f6c047bf4b1aac20172d2a6957a788ad27b11")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project21032025")]
